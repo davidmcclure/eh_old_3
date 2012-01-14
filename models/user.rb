2fc1958cfdbd48523be2139099b4bc2379531c9b
David McClure <david.mcclure@virginia.edu>
2012-01-14 11:08:20 -0500

    starting admin tests

--- models/user.rb
+++ models/user.rb
@@ -7,6 +7,16 @@
 class User < Ohm::Model
 
   attribute   :username
+  attribute   :password
+  attribute   :salt
+  attribute   :email
   collection  :poems, Poem
+
+  def validate
+    assert_present :username
+    assert_present :password
+    assert_present :salt
+    assert_present :email
+  end
 
 end
